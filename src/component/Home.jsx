071f48b3f2bd7534dedcf0d19b871aa4b348bab2
naushadalammtd871 <naushadalammtd871@gmail.com>
2025-09-30 09:25:11 +0530

    Update Home.jsx

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -7,14 +7,14 @@ const Home = () => {
   return (
     <div className='flex items-center mt-20 sm:mt-40 justify-center flex-col gap-10'>
         <motion.div initial={{opacity: 0, y: -500}} animate={{opacity: 1, y: 0}} transition={{duration: 2}} className="">
-            <h1 className='text-emerald-600 sm:text-3xl text-2xl font-bold'>Hello developer</h1>
+            <h1 className='text-emerald-600 text-3xl font-bold'>Hello developer</h1>
         </motion.div>
         <motion.div initial={{opacity: 0, x: -500}} animate={{opacity: 1, x: 0}} transition={{duration: 2}} className="flex items-center justify-center">
             <p className='sm:text-xl text-sm w-full sm:w-3/4 text-center text-[#ffffffc4]'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus consequuntur repellendus vitae laboriosam sed nemo incidunt, distinctio reprehenderit tempore sint iste accusamus vero est quos error officia illum ipsam odio quod hic nihil eius expedita nulla!
+                Weather is the state of the atmosphere, describing for example the degree to which it is hot or cold, wet or dry, calm or stormy, clear or cloudy. On Earth, most weather phenomena occur in the lowest layer of the planet's atmosphere, the troposphere, just below the stratosphere.
             </p>
         </motion.div>
-        <motion.button onClick={() => navigate("/label1")} initial={{opacity: 0, x: 500}} animate={{opacity: 1, x: 0}} transition={{duration: 2}} className='text-xl font-bold text-[#333333] bg-emerald-600 py-[10px] px-[30px] rounded-full'>Explore</motion.button>
+        <motion.button onClick={() => navigate("/label1")} initial={{opacity: 0, x: 500}} animate={{opacity: 1, x: 0}} transition={{duration: 2}} className='text-xl mt-5 sm:mt-0 font-bold text-[#333333] bg-emerald-600 py-[10px] px-[30px] rounded-full'>Explore</motion.button>
     </div>
   )
 }
