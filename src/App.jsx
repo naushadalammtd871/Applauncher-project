196348271a6db530f236fa779236ed345a77fcd2
naushadalammtd871 <naushadalammtd871@gmail.com>
2025-09-30 09:31:16 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Navbar from './component/Navbar'
 
 const App = () => {
   return (
-    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+    <div className='px-1 sm:px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
       <BrowserRouter>
       <Navbar />
         <Routes>
